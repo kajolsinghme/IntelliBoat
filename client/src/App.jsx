91641cf0e2dcba1391f6a618ffaa1f335886b0fa
kajolsinghme <kajolsingh985@gmail.com>
2025-09-07 19:59:18 +0530

    Setup project structure and added Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />}></Route>
+            <Route path="/credits" element={<Credits />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;

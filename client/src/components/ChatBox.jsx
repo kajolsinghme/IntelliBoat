a5963d77b5eb5fcc3df24e8a3649fb7b27749433
kajolsinghme <kajolsingh985@gmail.com>
2025-09-17 23:59:04 +0530

    Initialize ChatBox with selected chat messages

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 
 const ChatBox = () => {
@@ -7,8 +7,20 @@ const ChatBox = () => {
   const[messages, setMessages] =  useState([])
   const[loading, setLoading] =  useState(false)
 
+  useEffect(() => {
+    if(selectedChat){
+      setMessages(selectedChat.messages)
+    }
+  },[selectedChat])
   return (
-    <div></div>
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
+      {/* Chat Messages */}
+      <div className='flex-1 mb-5 overflow-y-scroll'>
+
+      </div>
+      {/* Prompt Input Box */}
+      <form action=""></form>
+    </div>
   )
 }
 
